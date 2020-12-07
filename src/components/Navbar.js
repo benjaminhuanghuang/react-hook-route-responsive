95851b65c52777cebc4b610591f093f424e036fe
benjaminhuanghuang <bhuang@rms.com>
2020-12-06 21:03:22 -0800

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { MdFingerprint } from "react-icons/md";
 //
-import './Navbar.css'
+import "./Navbar.css";
 
 //
-import {Button} from './Button'
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
-
 
   const handleClick = () => {
     setClick(!click);
@@ -24,6 +23,13 @@ function Navbar() {
       setButton(true);
     }
   };
+
+  useEffect(() => {
+    showButton();
+    window.addEventListener("resize", showButton);
+    return () => window.removeEventListener("resize", showButton);
+  }, []);
+
   return (
     <>
       <div className="navbar">
@@ -37,28 +43,30 @@ function Navbar() {
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            <Link to="/" className="nav-liks">
+            <Link to="/" className="nav-links">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/services" className="nav-liks">
+            <Link to="/services" className="nav-links">
               Services
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/products" className="nav-liks">
+            <Link to="/products" className="nav-links">
               Products
             </Link>
           </li>
           <li className="nav-btn">
             {button ? (
-              <Link to="/sign-up" className="but-link">
-                <Button buttonStyle="btn-outline">Sign Up</Button>
+              <Link to="/sign-up" className="btn-link">
+                <Button buttonStyle="btn--outline">Sign Up</Button>
               </Link>
             ) : (
-              <Link to="/" className="but-link">
-                <Button buttonStyle="btn-outline" buttonSize='btn--mobile'>Sign Up</Button>
+              <Link to="/" className="btn-link">
+                <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                  Sign Up
+                </Button>
               </Link>
             )}
           </li>
